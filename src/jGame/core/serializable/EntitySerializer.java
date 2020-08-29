004e87c027d705dbf062b9b351076bc721048832
Naapperas <nunoafonso2002@gmail.com>
2020-08-29 01:06:00 +0100

    Added serialization foundations
    Added code to implement serialization.

---
+++ src/jGame/core/serializable/EntitySerializer.java
@@ -0,0 +1,12 @@
+package jGame.core.serializable;
+
+/**
+ * 
+ * Class responsible for serializing entities.
+ * 
+ * @author Nuno Pereira
+ * @since 1.1.0
+ */
+public class EntitySerializer {
+
+}
---
+++ src/jGame/core/serializable/GameSerializer.java
@@ -0,0 +1,12 @@
+package jGame.core.serializable;
+
+/**
+ * Class responsible for the serialization of the game in its current state.
+ * Delegates methods to other serializers.
+ * 
+ * @author Nuno Pereira
+ * @since 1.1.0
+ */
+public class GameSerializer {
+
+}
