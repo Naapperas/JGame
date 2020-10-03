69ac226008f82065ea71970b92c6d3c8fe1448fe
Naapperas <nunoafonso2002@gmail.com>
2020-10-03 01:09:47 +0100

    Starting progress on scroll view element
    Basicly same code as dropdown element, wihtout the dropdown part

---
+++ src/jGame/core/ui/hud/UIHudScrollViewElement.java
@@ -0,0 +1,39 @@
+package jGame.core.ui.hud;
+
+import java.awt.Graphics2D;
+
+public class UIHudScrollViewElement extends UIHudElement {
+
+	public UIHudScrollViewElement() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UIHudScrollViewElement(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UIHudScrollViewElement(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void render(Graphics2D g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void registerInputListener() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeInputListener() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
