6598c7733c577ba077af480553021c0e49c2044e
Naapperas <nunoafonso2002@gmail.com>
2020-11-14 15:48:40 +0000

    Added javatuples dependancy for use of tuples in Java

--- src/jGame/core/sound/SoundStore.java
+++ src/jGame/core/sound/SoundStore.java
@@ -4,54 +4,115 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import org.javatuples.Pair;
+
+import jGame.logging.ProgramLogger;
 
 public class SoundStore {
+
+	public static void init() {
+
+		String resourcePath = "audio/";
+		
+		InputStream audioInPath = getDataStream(resourcePath);
+		
+		if (audioInPath == null) { ProgramLogger.writeLog("No audio files detected"); }
+		else {
+			try {
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(audioInPath));
+				String resource;
+
+				while ((resource = br.readLine()) != null) {
+					if (resource.endsWith(".wav")) {
+						// is .wav file, proceed
+						String soundName = resource.split(".")[0];
+						cacheSound(soundName, getSoundData(resourcePath + resource));
+					}
+				}
+
+				audioInPath.close();
+			} catch (IOException e) {
+				ProgramLogger.writeErrorLog(e);
+			}
+		}
+	}
 
 	private SoundStore() {
 		// make non-instantiable
 	}
 
-	private static HashMap<String, byte[]> soundMap = new HashMap<String, byte[]>();
-
-	public static void cacheSound(String soundIdentifier, byte[] soundData) {
-		soundMap.put(soundIdentifier, soundData);
-	}
-
-	// CODE TAKEN FROM
-	// https://stackoverflow.com/questions/3923129/get-a-list-of-resources-from-classpath-directory
+	private static HashMap<String, Pair<AudioFormat, byte[]>> soundMap = new HashMap<String, Pair<AudioFormat, byte[]>>();
 
 	/**
-	 * List all of the entries (files or not) in the given <i>classpath</i> path.
 	 * 
-	 * See <a href="
-	 * https://stackoverflow.com/questions/3923129/get-a-list-of-resources-from-classpath-directory">this
-	 * post</a> for more information on the behavior of this method
-	 * 
-	 * @param path the path of the classpath resource
-	 * @return a list of Strings denoting all the entries in the given path
-	 * @throws IOException if n IOException occurs
-	 * @author iirekm@StackOverflow.com
-	 * @since 1.3.0
+	 * @param soundIdentifier
+	 * @param soundData
+	 * @param format
 	 */
-	public static List<String> getResourceFiles(String path) throws IOException {
-		List<String> filenames = new ArrayList<>();
-
-		try (InputStream in = getResourceAsStream(path);
-				BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-			String resource;
-
-			while ((resource = br.readLine()) != null) { filenames.add(resource); }
-		}
-
-		return filenames;
+	public static void cacheSound(String soundIdentifier, byte[] soundData, AudioFormat format) {
+		cacheSound(soundIdentifier, Pair.with(format, soundData));
 	}
 
-	private static InputStream getResourceAsStream(String resource) {
-		final InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource);
+	/**
+	 * 
+	 * @param soundIdentifier
+	 * @param audio
+	 */
+	private static void cacheSound(String soundIdentifier, Pair<AudioFormat, byte[]> audio) {
+		soundMap.put(soundIdentifier, audio);
+	}
 
-		return in == null ? SoundStore.class.getResourceAsStream(resource) : in;
+	/**
+	 * 
+	 * @param soundIdentifier
+	 * @return the byte data of the sound to play
+	 * @since 1.3.0
+	 */
+	public static byte[] getSound(String soundIdentifier) {
+		return (byte[]) soundMap.get(soundIdentifier).getValue(1);
+	}
+
+	/**
+	 * 
+	 * @param resource
+	 * @return
+	 */
+	private static InputStream getDataStream(String resource) {
+
+		InputStream audioInPath = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource);
+
+		return (audioInPath == null ? SoundStore.class.getResourceAsStream(resource) : audioInPath);
+
+	}
+
+	/**
+	 * 
+	 * @param resource
+	 * @return the sound data in the given resource
+	 */
+	public static Pair<AudioFormat, byte[]> getSoundData(String resource) {
+		byte[] sound = null;
+		AudioFormat format = null;
+
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(getDataStream(resource));
+
+			sound = ais.readAllBytes();
+
+			format = ais.getFormat();
+
+			ais.close();
+		} catch (UnsupportedAudioFileException | IOException e) {
+			ProgramLogger.writeErrorLog(e);
+		}
+		return Pair.with(format, sound);
 	}
 }
